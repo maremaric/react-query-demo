923dcca25ed0e459feff374b78372f5482c44ad3
maremaric <markoreus888@gmail.com>
2024-05-07 16:36:07 +0200

    first commit

---
+++ src/components/DependentQueries.page.js
@@ -0,0 +1,3 @@
+export const DependentQueriesPage = () => {
+    return <div>Dependent queries</div>
+}
---
+++ src/components/RQSuperHeroes.page.js
@@ -0,0 +1,39 @@
+import { Link } from 'react-router-dom';
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
+
+export const RQSuperHeroesPage = () => {
+  const onSuccess = (data) => {
+    console.log('Perform side effect after data fetching', data);
+  }
+
+  const onError = (error) => {
+    console.log('Perform side effect after encountering error', error);
+  }
+
+  const { isLoading, data, isError, error, isFetching, refetch } = useSuperHeroesData(onSuccess, onError);
+
+  if (isLoading || isFetching) {
+    return <h2>Loading...</h2>
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>
+  }
+
+  return (
+    <>
+      <h2>React Query Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch heroes</button>
+      {data?.data.map((hero) => {
+        return (
+          <div key={hero.id}>
+            <Link to={`/rq-super-heroes/${hero.id}`}>{hero.name}</Link>
+          </div>
+        )
+      })}
+      {/* {data.map((heroName) => {
+        return <div key={heroName}>{heroName}</div>
+      })} */}
+    </>
+  )
+}
